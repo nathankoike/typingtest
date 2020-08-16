672c30ed38b5d7dffc7a3aa6f6a766062271ac0a
Nate Koike <nkoike1998@gmail.com>
2020-08-16 18:14:57 +0900

    now generating pseudo random words from a separate file

--- api/app.js
+++ api/app.js
@@ -1,12 +1,14 @@
 const fs = require("fs");
 const path = require("path");
 const express = require("express");
+const words = require("./generate_words");
 
 const port = 3000;
 const app = express();
 
 // catch all
 app.get("*", (req, res) => {
+  console.log(words.words(10));
   res.sendFile(path.join(__dirname, "../src/index.html"));
 });
 
